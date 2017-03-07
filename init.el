623dba9ce0776ff90a5276bf34b8422e54dcfd67
Violet Baddley <violet@masteryconnect.com>
2017-03-06 17:09:59 -0700

    rearranges emacs init order to be more correct

--- init.el
+++ init.el
@@ -1,186 +1,12 @@
-;; TOTE – The Only Text Encoding
-(set-language-environment 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-selection-coding-system 'utf-8)
-(set-locale-environment "en_US.UTF-8")
-(prefer-coding-system 'utf-8)
+(require 'package)
 
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(if (display-graphic-p)
-    (progn  ;; has graphical system
-      (desktop-save-mode 1)
-      (mac-auto-operator-composition-mode))
-  (progn    ;; isatty
-    (menu-bar-mode -1)
-    (xterm-mouse-mode 1)
-    (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))))
+; activate all the packages (in particular autoloads)
+(setq package-enable-at-startup nil)
+(package-initialize)
 
-(setq mac-option-modifier 'meta
-      mac-right-option-modifier nil
-      mac-command-modifier 'super)
-
-
-(global-set-key (kbd "s-c") 'kill-ring-save)
-(global-set-key (kbd "s-x") 'kill-region)
-(global-set-key (kbd "s-v") 'yank)
-(global-set-key (kbd "s-z") 'undo)
-(global-set-key (kbd "M-s-t") 'helm-projectile-switch-project)
-(global-set-key (kbd "s-t") 'helm-projectile-find-file)
 
 (add-to-list 'load-path "~/.emacs.d/themes/")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-					;(require 'color-theme-tomorrow)
-					;(load-theme 'tomorrow-night)
-
-
-
-
-
-(require 'package)
-
-; list the packages you want
-(setq package-list '(dash
-                     enh-ruby-mode
-                     yaml-mode
-                     json-mode
-                     web-mode
-                     fish-mode
-                     helm-projectile
-                     nyan-mode
-                     neotree
-                     sublimity
-                     anzu
-                     window-numbering
-                     smartparens))
-
-; list the repositories containing them
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
-
-; activate all the packages (in particular autoloads)
-(package-initialize)
-
-; fetch the list of packages available
-(unless package-archive-contents
-  (package-refresh-contents))
-
-; install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-
-
-
-
-
-(setq vc-follow-symlinks t)
-
-(projectile-global-mode)
-(setq projectile-enable-caching t)
-
-(require 'helm-config)
-(helm-mode 1)
-
-(define-key global-map [remap find-file] 'helm-find-files)
-(define-key global-map [remap occur] 'helm-occur)
-(define-key global-map [remap list-buffers] 'helm-buffers-list)
-(define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(unless (boundp 'completion-in-region-function)
-  (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
-  (define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point))
-;;(add-hook 'kill-emacs-hook #'(lambda () (and (file-exists-p "/tmp/helm-cfg.el") (delete-file "/tmp/helm-cfg.el"))))
-
-(defun scroll-half-page-down ()
-  "scroll down half the page"
-  (interactive)
-  (scroll-down (/ (window-body-height) 2))
-  (move-to-window-line nil))
-
-(defun scroll-half-page-up ()
-  "scroll up half the page"
-  (interactive)
-  (scroll-up (/ (window-body-height) 2))
-  (move-to-window-line nil))
-
-(global-set-key [remap scroll-down-command] 'scroll-half-page-down)
-(global-set-key [remap scroll-up-command] 'scroll-half-page-up)
-
-
-(require 'helm-projectile)
-(helm-projectile-on)
-
-(add-to-list 'auto-mode-alist
-             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
-
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-
-
-
-(require 'sublimity)
-;; (require 'sublimity-attractive)
-;; sublimity-map configured conditionally near end
-;; sublimity-mode enabled thereafter
-
-(require 'sublimity-scroll)
-(setq sublimity-scroll-weight 10
-      sublimity-scroll-drift-length 3)
-
-
-(require 'smartparens-config)
-(add-hook 'prog-mode-hook #'smartparens-mode)
-(add-hook 'prog-mode-hook #'show-smartparens-mode)
-
-
-
-(require 'web-mode)
-
-(defun my-web-mode-hook ()
-  (setq web-mode-enable-auto-pairing nil)  ; to be compatible with smartparens
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
-
-(add-hook 'web-mode-hook  'my-web-mode-hook)
-
-(defun sp-web-mode-is-code-context (id action context)
-  (and (eq action 'insert)
-       (not (or (get-text-property (point) 'part-side)
-                (get-text-property (point) 'block-side)))))
-
-;; end smartparens-web-mode support
-
-
-
-(global-anzu-mode +1)
-
-(setq make-backup-files nil)
-(setq-default indent-tabs-mode nil)
-(setq js-indent-level 2)
-(setq js2-basic-offset 2)
-(setq css-indent-offset 2)
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-(global-git-commit-mode)
-(global-auto-revert-mode 1)
-
-(window-numbering-mode)
-(defface window-numbering-face '((default :weight extra-bold :foreground "RoyalBlue1"))
-  "Face for window number in the mode-line.")
-
-
-
-(if (display-graphic-p)
-    (progn
-      (nyan-mode)
-      (require 'sublimity-map)
-      (sublimity-map-set-delay nil)))
-
-(sublimity-mode 1)
-
-
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -236,3 +62,172 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Hasklig" :foundry "nil" :slant normal :weight medium :height 120 :width normal))))
  '(highlight ((t (:background "gray20")))))
+
+
+
+
+;;
+;; END CUSTOM
+;;
+
+
+
+
+;; TOTE – The Only Text Encoding
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-selection-coding-system 'utf-8)
+(set-locale-environment "en_US.UTF-8")
+(prefer-coding-system 'utf-8)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(if (display-graphic-p)
+    (progn  ;; has graphical system
+      (desktop-save-mode 1)
+      (mac-auto-operator-composition-mode))
+  (progn    ;; isatty
+    (menu-bar-mode -1)
+    (xterm-mouse-mode 1)
+    (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))))
+
+(setq mac-option-modifier 'meta
+      mac-right-option-modifier nil
+      mac-command-modifier 'super)
+
+
+(global-set-key (kbd "s-c") 'kill-ring-save)
+(global-set-key (kbd "s-x") 'kill-region)
+(global-set-key (kbd "s-v") 'yank)
+(global-set-key (kbd "s-z") 'undo)
+(global-set-key (kbd "M-s-t") 'helm-projectile-switch-project)
+(global-set-key (kbd "s-t") 'helm-projectile-find-file)
+
+
+
+
+
+
+; list the packages you want
+(setq package-list '(dash
+                     enh-ruby-mode
+                     yaml-mode
+                     json-mode
+                     web-mode
+                     fish-mode
+                     helm-projectile
+                     nyan-mode
+                     neotree
+                     anzu
+                     window-numbering
+                     smartparens))
+
+; list the repositories containing them
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+
+
+
+(setq vc-follow-symlinks t)
+
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+
+(require 'helm-config)
+(helm-mode 1)
+
+(define-key global-map [remap find-file] 'helm-find-files)
+(define-key global-map [remap occur] 'helm-occur)
+(define-key global-map [remap list-buffers] 'helm-buffers-list)
+(define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(unless (boundp 'completion-in-region-function)
+  (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
+  (define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point))
+;;(add-hook 'kill-emacs-hook #'(lambda () (and (file-exists-p "/tmp/helm-cfg.el") (delete-file "/tmp/helm-cfg.el"))))
+
+(defun scroll-half-page-down ()
+  "scroll down half the page"
+  (interactive)
+  (scroll-down (/ (window-body-height) 2))
+  (move-to-window-line nil))
+
+(defun scroll-half-page-up ()
+  "scroll up half the page"
+  (interactive)
+  (scroll-up (/ (window-body-height) 2))
+  (move-to-window-line nil))
+
+(global-set-key [remap scroll-down-command] 'scroll-half-page-down)
+(global-set-key [remap scroll-up-command] 'scroll-half-page-up)
+
+
+(require 'helm-projectile)
+(helm-projectile-on)
+
+(add-to-list 'auto-mode-alist
+             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+
+
+
+
+(require 'smartparens-config)
+(add-hook 'prog-mode-hook #'smartparens-mode)
+(add-hook 'prog-mode-hook #'show-smartparens-mode)
+
+
+
+(require 'web-mode)
+
+(defun my-web-mode-hook ()
+  (setq web-mode-enable-auto-pairing nil)  ; to be compatible with smartparens
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+(defun sp-web-mode-is-code-context (id action context)
+  (and (eq action 'insert)
+       (not (or (get-text-property (point) 'part-side)
+                (get-text-property (point) 'block-side)))))
+
+;; end smartparens-web-mode support
+
+
+
+(global-anzu-mode +1)
+
+(setq make-backup-files nil)
+(setq-default indent-tabs-mode nil)
+(setq js-indent-level 2)
+(setq js2-basic-offset 2)
+(setq css-indent-offset 2)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(global-git-commit-mode)
+(global-auto-revert-mode 1)
+
+(window-numbering-mode)
+(defface window-numbering-face '((default :weight extra-bold :foreground "RoyalBlue1"))
+  "Face for window number in the mode-line.")
+
+
+
+(if (display-graphic-p)
+    (progn
+      (nyan-mode)
+      ))
