dbe28ecfc46544e1f98884d22824f069bc79eb0f
Violet Baddley <violet@masteryconnect.com>
2018-09-24 20:02:09 +0000

    reverts a change wherein I accidentally unhid the menubar over tty

--- init.el
+++ init.el
@@ -144,6 +144,7 @@
           (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))))))
 
   (progn    ;; isatty
+    (menu-bar-mode -1)
     (xterm-mouse-mode 1)
     (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
     (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
