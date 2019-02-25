3f5b9f1f52be77a5b0402f285af83c14d4688779
Violet Baddley <violet@masteryconnect.com>
2019-02-25 23:38:08 +0000

    sets avy to jump only within the current window

--- init.el
+++ init.el
@@ -212,7 +212,8 @@
 (use-package avy
   :ensure t  ;; This should be built-in, not sure why it's not always available
   :init (progn
-          (setq avy-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n ?s))
+          (setq avy-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n ?s)
+                avy-all-windows nil)
           (global-set-key (kbd "C-z") 'avy-goto-char)
           (global-set-key (kbd "M-z") 'avy-goto-end-of-line)
           (global-set-key (kbd "C-M-z") 'avy-goto-word-0))
