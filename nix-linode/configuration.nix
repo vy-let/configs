e8db2ec18bd676fa8162537791974bf71e0728e1
Vy-let <violet@violetbaddley.com>
2020-08-09 05:47:45 +0000

    add details on making the swapfile

--- nix-linode/configuration.nix
+++ nix-linode/configuration.nix
@@ -22,6 +22,11 @@
 
   # Ya gotta take this out of the hardware config. You won't forget,
   # nix'll yell at you otherwise.
+  #
+  # dd if=/dev/zero of=/mnt/swapfile bs=1M count=1024
+  # chmod 0600 /mnt/swapfile
+  # mkswap /mnt/swapfile
+  # swapon /mnt/swapfile
   swapDevices = [{
     device = "/swapfile";
     size = 1024;
