5c936f7cddbaa7f9257aa3357523e3a34d5335f0
Vy-let Software <violet@violetbaddley.com>
2020-08-30 22:02:13 -0600

    integrate emacs config into home-manager setup

--- nix/home/emacs.nix
+++ nix/home/emacs.nix
@@ -1,9 +1,54 @@
 { pkgs, ... }:
 
 {
+  home.file.emacs = {
+    source = ../../emacs/init.el;
+    target = ".emacs.d/init.el";
+  };
+
   programs.emacs = {
     enable = true;
     # package set in graphical/headless sprinkles
+
+    extraPackages = epkgs: with epkgs; [
+
+      base16-theme
+      use-package
+
+      avy
+      shackle
+      smart-mode-line
+      smart-mode-line-powerline-theme
+      rainbow-delimiters
+      anzu
+      winum
+      adaptive-wrap
+
+      helm
+      projectile
+      helm-projectile
+      magit
+      treemacs
+      treemacs-projectile
+      perspective
+
+      enh-ruby-mode
+      rjsx-mode
+      web-mode
+      swift-mode
+      haskell-mode
+      yaml-mode
+      json-mode
+      fish-mode
+      markdown-mode
+      vlf
+      nix-mode
+      terraform-mode
+      go-mode
+
+      org
+
+    ];
   };
 
   services.emacs = {
