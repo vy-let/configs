d2f4b707b9f41e879c061c7095b160d406da4f8a
Vy-let Software <violet@violetbaddley.com>
2021-12-29 17:02:07 -0700

    fix some graphics issues on thinkpad

--- nix/sprinkles/thinkpad.nix
+++ nix/sprinkles/thinkpad.nix
@@ -6,12 +6,19 @@
   ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.prime = {
+    # Choose sync xor offload
+    # https://forums.developer.nvidia.com/t/prime-and-prime-synchronization/44423
+    # offload.enable = true;
     sync.enable = true;
+
     nvidiaBusId = "PCI:1:0:0";
     intelBusId = "PCI:0:2:0";
   };
+
+  # This helps avoid tearing in "sync" mode, but sometimes (?) gives
+  # mouse cursor glitches
+  hardware.nvidia.modesetting.enable = true;
 
   services.xserver.wacom.enable = true;
   hardware.trackpoint = {
