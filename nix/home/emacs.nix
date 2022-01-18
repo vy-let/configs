49c7c28a8f8edb3ca2f378dcf1edb59d814d744e
Vy-let Software <violet@violetbaddley.com>
2022-01-17 22:47:26 -0700

    remove static nix emacs configuration

--- nix/home/emacs.nix
+++ nix/home/emacs.nix
@@ -1,54 +1,9 @@
 { pkgs, ... }:
 
 {
-  home.file.emacs = {
-    source = ../../emacs/init.el;
-    target = ".emacs.d/init.el";
-  };
-
   programs.emacs = {
     enable = true;
     # package set in graphical/headless sprinkles
-
-    extraPackages = epkgs: with epkgs; [
-
-      base16-theme
-      use-package
-
-      avy
-      shackle
-      smart-mode-line
-      smart-mode-line-powerline-theme
-      rainbow-delimiters
-      anzu
-      winum
-      # adaptive-wrap # Uncomment this later when elpa is back up
-
-      helm
-      projectile
-      helm-projectile
-      magit
-      treemacs
-      treemacs-projectile
-      perspective
-
-      enh-ruby-mode
-      rjsx-mode
-      web-mode
-      swift-mode
-      haskell-mode
-      yaml-mode
-      json-mode
-      fish-mode
-      markdown-mode
-      vlf
-      nix-mode
-      terraform-mode
-      go-mode
-
-      org
-
-    ];
   };
 
   services.emacs = {
@@ -61,4 +16,13 @@
     EDITOR = "emacsclient -t";
     ALTERNATE_EDITOR = "emacs";
   };
+
+  home.packages = with pkgs; [
+    # Doom-Emacs dependencies
+    ripgrep
+    fd
+
+    # LSP dependencies
+    rubyPackages.solargraph
+  ];
 }
