d038c0c27b0094e91aae8d8ea37f415cd790937d
Vy-let Software <violet@violetbaddley.com>
2022-11-19 17:25:31 -0800

    fix a typo

--- nix/bespoke-packages/mastodon/package.nix
+++ nix/bespoke-packages/mastodon/package.nix
@@ -54,8 +54,7 @@ in stdenv.mkDerivation rec {
 
     buildPhase = ''
       export HOME=$PWD
-      echo "NODE_OPTIONS WAS: $NODE_OPTIONS"
-      export export NODE_OPTIONS=--openssl-legacy-provider
+      export NODE_OPTIONS=--openssl-legacy-provider
       fixup_yarn_lock ~/yarn.lock
       yarn config --offline set yarn-offline-mirror ${yarnOfflineCache}
       yarn install --offline --frozen-lockfile --ignore-engines --ignore-scripts --no-progress
